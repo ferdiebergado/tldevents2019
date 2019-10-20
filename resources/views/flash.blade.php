49b64ce69dda9c2693a704320d160ab6a9588a1e
ferdie <ferdiebergado@gmail.com>
2019-10-20 12:03:37 +0800

    Added flash messages

---
+++ resources/views/flash.blade.php
@@ -0,0 +1,47 @@
+@if (session()->has('success'))
+    
+<div class="alert alert-success alert-dismissible fade show" role="alert">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        <span class="sr-only">Close</span>
+    </button>
+    <strong>{{ session('success') }}</strong>
+</div>
+
+@endif
+
+@if (session()->has('error'))
+    
+<div class="alert alert-danger alert-dismissible fade show" role="alert">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        <span class="sr-only">Close</span>
+    </button>
+    <strong>{{ session('error') }}</strong>
+</div>
+
+@endif
+
+@if (session()->has('info'))
+    
+<div class="alert alert-info alert-dismissible fade show" role="alert">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        <span class="sr-only">Close</span>
+    </button>
+    <strong>{{ session('info') }}</strong>
+</div>
+
+@endif
+
+@if (session()->has('warning'))
+    
+<div class="alert alert-warning alert-dismissible fade show" role="alert">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        <span class="sr-only">Close</span>
+    </button>
+    <strong>{{ session('warning') }}</strong>
+</div>
+
+@endif
